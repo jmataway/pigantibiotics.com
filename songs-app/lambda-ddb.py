4b907a5acf9d6ff50c0cd3aa9ae1337540b58bd3
Joel Mataway <jmataway@yahoo.com>
2020-05-23 09:28:24 -0700

    Updated with additional code from AWS docs

--- songs-app/lambda-ddb.py
+++ songs-app/lambda-ddb.py
@@ -1,8 +1,20 @@
 import boto3
 import random
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+from botocore.exceptions import ClientError
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
 
 firstId = 1
-
 lastId = 3
 
 dynamodb = boto3.resource("dynamodb", region_name='us-east-1')
@@ -11,15 +23,22 @@ table = dynamodb.Table('Songs')
 
 id = random.randint(firstId,lastId)
 
-response = table.get_item(
+try:
+    response = table.get_item(
         Key={
             'ID': id,
         }
     )
+except ClientError as e:
+    print(e.response['Error']['Message'])
+else:
+    item = response['Item']
+    print("Song retrieval succeeded:")
+    print(json.dumps(item, indent=4, cls=DecimalEncoder))
 
-item = response['Item']
+""" item = response['Item']
 print(item)
-print(type(item))
+print(type(item)) """
 
 """above works to pull a specific item. What needs to happen:
 3. Improve the above boto3 and ddb code, using: https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.03.html as a starting guide
