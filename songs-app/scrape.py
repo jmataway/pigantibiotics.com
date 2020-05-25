e05d3ca265e79e4f3fda576f3f2a0e2f8f35d9c9
Joel Mataway <jmataway@yahoo.com>
2020-05-25 10:16:16 -0700

    Added initial scrape.py

--- songs-app/scrape.py
+++ songs-app/scrape.py
@@ -1,1 +1,20 @@
-# file to start python web scraping for song info
+import urllib
+import requests
+from bs4 import BeautifulSoup
+
+# desktop user-agent
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+# mobile user-agent
+MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+
+query = "fitter happier radiohead"
+query = query.replace(' ', '+')
+URL = f"https://google.com/search?q={query}"
+
+headers = {"user-agent": USER_AGENT}
+r = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(r.text, 'lxml')
+
+result = soup.find('div', class_='zloOqf PZPZlf')
+print(result.text)
